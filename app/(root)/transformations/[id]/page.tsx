4f53147063674924650cefdbaf0b8d44df89855f
williebonavente <williebonavente@gmai.com>
2025-06-25 19:40:05 +0800

    Init project

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TransformationPage = () => {
+  return (
+    <div>
+      TransformationPage
+    </div>
+  )
+}
+
+export default TransformationPage 
+
+
+// Dynamic Routing 
---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,42 @@
+// clerkId, email, user, photo, firstName, lastName, planId, creditBalance
+import {Document, model, models, Schema } from 'mongoose';
+
+
+const UserSchema = new Schema({
+    clerkId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    
+    photo: {
+        type: String,
+        required: true,
+    },
+
+    planId: {
+        type: Number,
+        default: 1
+    },
+
+    creditBalance: {
+        type: Number,
+        default: 10, 
+    }
+});
+
+
+const User = models?.User || model("User", UserSchema);
+export default User;
