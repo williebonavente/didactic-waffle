0e755d5b15cde7f965c8392098059c699849ee63
williebonavente <williebonavente@gmai.com>
2025-06-25 22:01:25 +0800

    Middleware fix

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublic = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+const isPublic = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhooks/clerk.(*)']);
 
 
 export default clerkMiddleware(async (auth, req) => {
-  publicRoutes: ['/api/webhooks/clerk']
   if (!isPublic(req)) {
     await auth.protect()
   }
